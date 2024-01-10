b3ac2255df3862e021eff17cf50e07642b9d0e26
JoseVilla97 <101680984+JoseVilla97@users.noreply.github.com>
2024-01-10 17:09:27 -0600

    Update Calibracion_SOILSENS.ino

--- Calibracion_SOILSENS.ino
+++ Calibracion_SOILSENS.ino
@@ -5,75 +5,80 @@
 #include <TinyGsmClient.h> //bibliotecas para comunicacion
 #include <ThingerTinyGSM.h>
 #include <ThingerESP32.h> 
-#include <SimpleTimer.h>//biblioteca para el timer
+#include <Simpletimer.h>//biblioteca para el timer
 
-const float dry = 2.80; // value for dry soil
-const float wet = 1.20; // value for wet soil
+const float dry = 2.80; // Voltage value for dry soil
+const float wet = 1.20; // Voltage value for wet soil
 
-
-#define sensor_PIN 25
+#define Soil_PIN 25
 #define RXD1 27 //Segun la guia de pines del fabricante estos son los pines TX Y RX
 #define TXD1 26
-float sensorVal;
-int percent_Humididy;
-float Contenido_Vol_real;
 
-SimpleTimer timer;//se declara el objeto timer
+float Voltaje;
+float soil_value ;
+int percent_Humididy;
+float Contenido_Vol_real; // contenido volumètrico real 
+float pendiente = 2.48; // slope from linear fit
+float intercepcion = -0.72; // intercept from linear fit
+
+Simpletimer timer1{};
 char data;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(sensor_PIN, INPUT);
-
+  pinMode(Soil_PIN, INPUT);
   pinMode(23, OUTPUT);
   digitalWrite(23, LOW);
   delay(1000);
   digitalWrite(23, HIGH);
   Serial2.begin(115200, SERIAL_8N1, TXD1, RXD1);//Es necesario invertir los pines para una correcta comunicacion UART
   delay(1000);
-  
-  timer.setInterval(5000, Sensor);
+  timer1.register_callback(Sensor);
 
 }
 
 
-
-void Sensor() { // a function to be executed periodically
-  sensorVal = analogRead(sensor_PIN);//recibe valores de entrada análoga, en este caso del sensor en el pin 25
-
-
+void Sensor() {
 /**pero para el ESP32 las entradas ADC o GPIO (int-out) trae por defecto 12 bits 
-Es importante recalcar que por defecto se trabaja a 12 bits, osea me que tiene un rango entre los (0-4095) 
-Pero podemos ajustarlo mediante código de 12 a 11 (0-2048) o 10 bits (0-1024) o bien de 9 bits (0-512)**/
-
-  Serial.print("Soil Moisture Sensor Voltage: ");
-  float Voltaje =sensorVal/4095*3.3;
+Es importante recalcar que por defecto se trabaja a 12 bits, osea me que tiene que dar un rango entre los (0-4095) 
+pero podemos ajustarlo mediante código de 12 a 11 (0-2048) o 10 bits (0-1024) o bien de 9 bits (0-512), sin emabargo para este caso no lo vamos a aplicar**/
+  
+  soil_value  = analogRead(Soil_PIN);//recibe valores de entrada análoga, en este caso del sensor en el pin 25
+  Serial.print("Soil Moisture Sensor Voltage: "); // Por medio de una simple regla de 3
+  Voltaje = soil_value /4095*3.3; //Calculo el voltaje basado en la lectura del ADC del ESP32, teniendo en cuenta que trabaja a 12 bits por defecto.
   Serial.print(Voltaje);  //bits leidos del sensor/los 12 bits *3.3Vol //12bits con tecnologpia ttl sería 0-4095 
   Serial.println(" V");// lo ocupo en voltage
-  ////Formulas es θv=(2,48/V)-0,72
-//  Contenido_Vol_real=(2,48/Voltaje)-0,72;
-////convertir a hunedad
-  //porcentaje = (Contenido_Vol_real / Contenido_Vol_real_MAX) * 100;//convertir el voltage a porcentaje
-//  Serial.print("Soil moisture real: ");
-//  Serial.print(Contenido_Vol_real);  //bits leidos del sensor/los 12 bits *3.3Vol //12bits con tecnologpia ttl sería 0-4095 
-//  Serial.println(" θv");// lo ocupo en voltage
+  
+  //Formulas es θv=(a/V)+b //lectura de tensión tomada de nuestro sensor electromagnético puede relacionarse con el contenido volumétrico de agua mediante un ajuste lineal
+  Contenido_Vol_real=(pendiente/Voltaje)+intercepcion; //Calculo de contenido volumètrico de agua
+  Serial.print("Contenido volumetrico de agua en suelo: ");
+  Serial.print(Contenido_Vol_real);  //bits leidos del sensor/los 12 bits *3.3Vol //12bits con tecnologpia ttl sería 0-4095 
+  Serial.println(" cm^3/cm^3");// proporción de volumen de agua con respecto al volumen total del suelo
+  //1 cm³/cm³ (o 1 m³/m³) = 100% de humedad: Significa que todo el volumen del suelo está lleno de agua
+  //0 cm³/cm³ = 0% de humedad: Significa que no hay agua en el volumen del suelo. El suelo está completamente seco.
+
+
+  //En esta secciòn puedo calcular el porcentaje de humedad segun 
+  Serial.print("Soil Moisture bits: "); 
+  Serial.println(soil_value); 
 
   
-//  Serial.print("Soil Moisture bits: "); 
-//  Serial.println(sensorVal); 
-//  percent_Humididy = map(sensorVal, dry, wet, 0, 100);
-//  if (percent_Humididy >= 100) {
-//    percent_Humididy = 100;
-//  }
-//  else if (percent_Humididy <= 0) {
-//    percent_Humididy = 0;
-//  }
-//  Serial.print(" Soil moisture: ");
-//  Serial.println(percent_Humididy);
-
+  percent_Humididy = map(Voltaje, dry, wet, 0, 100); //Mapeo del valor del sensor a un porcentaje de humedad entre 0 y 100, donde dry es 0 y wet es 100.
+  if (percent_Humididy >= 100) {
+    percent_Humididy = 100;
+  }
+  else if (percent_Humididy <= 0) {
+    percent_Humididy = 0;
+  }
+  Serial.print("Soil moisture: ");
+  Serial.print(percent_Humididy);
+  Serial.println("%");
 }
 
+/*El contenido de agua en el punto de marchitez permanente (cuando las plantas ya no pueden extraer agua) 
+y en la capacidad de campo (máximo contenido de agua que el suelo puede retener después de un riego o lluvia)
+son valores clave a considerar para la gestión del agua en el suelo.*/
 
 void loop() {
-  timer.run();
+  timer1.run(5000);
 }
